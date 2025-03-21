fb26672eee094e6fcf08a1669a8b3f561c15bfd4
Roberto Manganelly <robmanganelly@gmail.com>
2025-03-21 00:35:18 -0400

    initial

--- hello.go
+++ hello.go
@@ -2,6 +2,29 @@ package main
 
 import "fmt"
 
+type person struct {
+	name  string
+	age   int
+	color string
+}
+
+var Fred person
+
+var pet = struct {
+	name string
+	kind string
+}{"Fido", "dog"}
+
 func main() {
-	fmt.Println("Hello, Go!")
+	Bill := person{}
+	Norma := person{"Norma", 25, "blue"}
+	Carol := person{name: "Carol", age: 30}
+	pet := 213
+
+	fmt.Println(Fred)
+	fmt.Println(Bill)
+	fmt.Println(Norma)
+	fmt.Printf("hello %s\n", Carol.name)
+	fmt.Printf("hello %s, congratulations on your %dth birthday!\n", Carol.name, Carol.age)
+	fmt.Println("my pet is", pet)
 }
